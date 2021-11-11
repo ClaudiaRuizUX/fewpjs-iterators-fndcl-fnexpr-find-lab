2a2f6b7ad9f7a6d811ddf27d3493e6ba210c38e5
Claudia <claudiaruizux@gmail.com>
2021-11-11 18:45:59 +1100

    Completed find

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+// 1) returns a year the KC Chiefs won the superbowl
+// 2) returns undefined if the record has no win objects
 
+superbowlWin = (record) => {
+    let result = record.find( record => record.result === "W" )
+    return !!result ? result.year : undefined
+  }
